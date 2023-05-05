fa4adb1040c7c4e082cc2b133bf26758ac6f9511
shuning <linshuning21@gmail.com>
2023-05-05 11:04:10 +0800

    initial commit

---
+++ marvel-backend/src/main/java/tfip/csf/marvelbackend/MarvelBackendApplication.java
@@ -0,0 +1,13 @@
+package tfip.csf.marvelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelBackendApplication.class, args);
+	}
+
+}
---
+++ marvel-backend/src/test/java/tfip/csf/marvelbackend/MarvelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tfip.csf.marvelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
